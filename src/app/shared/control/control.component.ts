6e561c4dcd9b55423329e1fdaac6d9fb3fefe49f
rauatorres <rauatorres@gmail.com>
2026-06-04 17:01:22 -0300

    adicionando multiplos class bindings no componente 'server-status'

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -14,8 +20,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
 })
 export class ControlComponent {
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
     console.log('clicked');
+    console.log(this.el);
   }
 }
