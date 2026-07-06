bb0c714a9b8a9ddc58b011af898d3614cb52f382
rauatorres <rauatorres@gmail.com>
2026-07-06 11:06:46 -0300

    variáveis de template

--- src/app/components/dashboard/dashboard-item-content/new-ticket/new-ticket.component.ts
+++ src/app/components/dashboard/dashboard-item-content/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/button/button.component';
 import { ControlComponent } from '../../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    console.log(titleElement.value);
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+
+    this.form().nativeElement.reset();
   }
 }
